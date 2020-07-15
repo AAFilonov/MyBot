92a8fbba95d445dbdfe3dea946f52e64f6a95c6a
GreatDarkLord <filon13p@gmail.com>
2020-07-15 18:54:30 +0300

    Test

--- VkBot/Controllers/CallbackController.cs
+++ VkBot/Controllers/CallbackController.cs
@@ -1,8 +1,13 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 using VkNet.Model;
 using VkNet.Utils;
+using VkNet.Abstractions;
+using VkNet.Model.RequestParams;
+
+
 
 namespace VkBot.Controllers
 {
@@ -14,12 +19,14 @@ namespace VkBot.Controllers
         /// Конфигурация приложения
         /// </summary>
         private readonly IConfiguration _configuration;
+        private readonly IVkApi _vkApi;
 
-        public CallbackController(IConfiguration configuration)
+     
+        public CallbackController(IVkApi vkApi, IConfiguration configuration)
         {
+            _vkApi = vkApi;
             _configuration = configuration;
         }
-
         [HttpPost]
         public IActionResult Callback([FromBody] Updates updates)
         {
@@ -32,13 +39,22 @@ namespace VkBot.Controllers
                     return Ok(_configuration["Config:Confirmation"]);
                 }
 
-                // Новое сообщение
+                    // Новое сообщение
+                   
                 case "message_new":
-                {
-                    // Десериализация
-                    var msg = Message.FromJson(new VkResponse(updates.Object));
-                    break;
-                }
+                    {
+                        // Десериализация
+                        var msg = Message.FromJson(new VkResponse(updates.Object));
+                        // ВОТ СДЕСЬ МЫ РЕШАЕМ ЧЕ ОТПРАВИТЬ ЫААЫ
+                        // Отправим в ответ полученный от пользователя текст
+                        _vkApi.Messages.Send(new MessagesSendParams
+                        {
+                            RandomId = new DateTime().Millisecond,
+                            PeerId = msg.PeerId.Value,
+                            Message = msg.Text
+                        });
+                        break;
+                    }
             }
 
             return Ok("ok");

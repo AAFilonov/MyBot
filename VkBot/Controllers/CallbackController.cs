6e530cbc03d02c453a08f92046a1144c057506ae
GreatDarkLord <filon13p@gmail.com>
2020-08-04 18:14:11 +0300

    Binary decoding coms prev

--- VkBot/Controllers/CallbackController.cs
+++ VkBot/Controllers/CallbackController.cs
@@ -4,11 +4,11 @@ using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 using VkNet.Model;
 using VkNet.Utils;
+using System.Text;
+using System.IO;
 using VkNet.Abstractions;
 using VkNet.Model.RequestParams;
-
-
-   
+using System.Text.RegularExpressions;
 
 namespace VkBot.Controllers
 {
@@ -57,26 +57,59 @@ namespace VkBot.Controllers
         async public void HandleAsync(Message msg)//обработка полученного сообщения
         {
 
-            // Отправим в ответ полученный от пользователя текст
-            
+
+            var tmp = msg.Text;
+            StringBuilder stringToRead = new StringBuilder();
+          
+
+            string Answer="";
+           // Regex[] Coms = new Regex[] {  new Regex(@"^/help\s?"), new Regex(@"^/Code\s?"), new Regex(@"^/Decode\s?") };
+          
+
+            switch (msg.Text)
+            {
+                case var someVal when new Regex(@"^/Help\s?", RegexOptions.IgnoreCase).IsMatch(someVal):
+                    Answer = "/Help *smth* - вывести справку по командам\n"
+                        + "/Code *smth* - перевести произвольную последовательноть *smth* в двоичный код\n"
+                        + "/Decode *smth* - раскодировать бинарную последовательноть *smth* в текст, если она является кодированным текстом\n";
+                    break;
+
+                case var someVal when new Regex(@"^/Code\s?", RegexOptions.IgnoreCase).IsMatch(someVal):
+                    Answer = CodeBin(msg.Text.Substring(new string("/Code\\s").Length));
+                    break;
+                case var someVal when new Regex(@"^/Decode\s?", RegexOptions.IgnoreCase).IsMatch(someVal):
+                    Answer = CodeBin(msg.Text.Substring(new string("/Decode\\s").Length));
+                    break;
+                default:
+                    Answer = "Ошибка, нераспознанная последовательность";
+                    break;
+
+            }
+
             await _vkApi.Messages.SendAsync(new MessagesSendParams
             {
                 RandomId = new DateTime().Millisecond,
                 PeerId = msg.PeerId.Value,
-                Message = "Your text: "+ msg.Text
+                Message = Answer
             });
 
         }
-            public void Handle(Message msg)
+        private string CodeBin(string s)
         {
-          
-            // Отправим в ответ полученный от пользователя текст
-            _vkApi.Messages.Send(new MessagesSendParams
+            return "Coded "+s;
+        }
+        private string DecodeBin(string s)
+        {
+            Regex IsBinaryCode = new Regex(@"^(([01]{8}\s?){2})+$");
+
+            if (IsBinaryCode.IsMatch(s))
             {
-                RandomId = new DateTime().Millisecond,
-                PeerId = msg.PeerId.Value,
-                Message = "Your text: " + msg.Text
-            });
+                return "Decoded " + s;
+            }
+            else
+            {
+                return "Ошибка декодирования";
+            }
         }
     }
 }
